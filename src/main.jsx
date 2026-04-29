763f37b5c6ca0304c83c194f6205fa9aaa79b21d
DEADKILLER-4810 <nahdeadkiller4810@gmail.com>
2026-04-29 13:04:35 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, HashRouter} from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-const basename = import.meta.env.PROD ? "/finance-manager" : "";
+
 createRoot(document.getElementById('root')).render(
-
-    <HashRouter basename={basename}>
-      <App />
-    </HashRouter>
-
+  <HashRouter>
+    <App />
+  </HashRouter>
 )
