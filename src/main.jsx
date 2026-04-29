04ecc6274aeed098b06a12ed1b7d2a4a83fc6d1a
DEADKILLER-4810 <nahdeadkiller4810@gmail.com>
2026-04-29 13:01:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, HashRouter} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-
+const basename = import.meta.env.PROD ? "/finance-manager" : "";
 createRoot(document.getElementById('root')).render(
 
-    <HashRouter basename="/finance-manager">
+    <HashRouter basename={basename}>
       <App />
     </HashRouter>
 
