9d56f81dcc4d9354899da802c99205337188de97
DEADKILLER-4810 <nahdeadkiller4810@gmail.com>
2026-05-15 22:52:35 +0530

    useeffect added to fix bug

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import app from '../FirebaseConfig';
 import { getAuth, signInWithEmailAndPassword,   signInWithRedirect,  signInWithPopup, GoogleAuthProvider } from "firebase/auth";
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { getAuth, createUserWithEmailAndPassword, updateProfile,signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import app from "../FirebaseConfig";
